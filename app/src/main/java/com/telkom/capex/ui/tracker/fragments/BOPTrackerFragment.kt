be1cdb733d2b8755c99cbc689a3c65644c6ed2ea
mohek <mohekkus@gmail.com>
2023-02-11 14:39:58 +0700

    ADD BOP Detail
    NOTE if you re-do or add something, you can add shortcut in login
    
    TODO add view detail BOP (need adapt)
    
    TODO add view bulk edit DOC
    TODO add progress view

--- app/src/main/java/com/telkom/capex/ui/tracker/fragments/BOPTrackerFragment.kt
+++ app/src/main/java/com/telkom/capex/ui/tracker/fragments/BOPTrackerFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.telkom.capex.R
@@ -43,9 +44,9 @@ class BOPTrackerFragment: Fragment() {
                         val itemview = holder.itemView
 
                         itemview.apply {
-                            findViewById<TextView>(R.id.textView49).text = "a$position"
+//                            findViewById<TextView>(R.id.textView49).text = "a$position"
                             setOnClickListener {
-
+                                findNavController().navigate(R.id.action_navigation_tracker_to_BOPDetailFragment)
                             }
                         }
                     }
