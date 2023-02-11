2ace7b599eba33d6e9a339d25718452d6c94c828
mohek <mohekkus@gmail.com>
2023-02-11 13:25:56 +0700

    ADD view adding to budget
    NOTE if you re-do or add something, you can add shortcut in login
    
    TODO add view detail BOP (need adapt)
    
    TODO add view bulk edit DOC
    TODO add progress view

--- app/src/main/java/com/telkom/capex/ui/budget/fragments/BudgetDetail.kt
+++ app/src/main/java/com/telkom/capex/ui/budget/fragments/BudgetDetail.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.telkom.capex.R
 import com.telkom.capex.databinding.FragmentOverviewBinding
 import com.telkom.capex.ui.budget.BudgetViewModel
@@ -39,7 +39,7 @@ class BudgetDetail: Fragment() {
                     override fun createFragment(position: Int): Fragment {
                         return when (position) {
                             0 -> BudgetData()
-                            else -> BudgetEdit()
+                            else -> BudgetMonthlyBAST()
                         }
                     }
                 }
@@ -47,27 +47,15 @@ class BudgetDetail: Fragment() {
                     currentItem = if (it > 0) childCount
                     else 0
                 }
-                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                    override fun onPageSelected(position: Int) {
-                        super.onPageSelected(position)
-                        when (position) {
-                            childCount ->
-                                overviewEdit.setImageResource(R.drawable.ic_baseline_pending_actions_24)
-                            else -> overviewEdit.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_alt_24)
-                        }
+                TabLayoutMediator(tabLayout, overviewPager) {tab, pos ->
+                    tab.text = when (pos) {
+                        0 -> "Monthly BAST"
+                        1 -> "SMILE"
+                        else -> null
                     }
-                })
+                }.attach()
                 overviewEdit.setOnClickListener {
-                    overviewPager.apply {
-                        if (currentItem == childCount) {
-                            currentItem = 0
-                            overviewEdit.setImageResource(R.drawable.ic_baseline_sentiment_satisfied_alt_24)
-                        }
-                        else {
-                            currentItem = childCount
-                            overviewEdit.setImageResource(R.drawable.ic_baseline_pending_actions_24)
-                        }
-                    }
+                    findNavController().navigate(R.id.action_budgetDetail_to_budgetEdit)
                 }
             }
         }
